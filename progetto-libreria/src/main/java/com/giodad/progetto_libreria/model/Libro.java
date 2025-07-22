04f40adf13d8b4e401f93af37aee511a672e6e74
GiovanniDadone <143901741+GiovanniDadone@users.noreply.github.com>
2025-07-22 16:16:30 +0200

    versione finale

--- progetto-libreria/src/main/java/com/giodad/progetto_libreria/model/Libro.java
+++ progetto-libreria/src/main/java/com/giodad/progetto_libreria/model/Libro.java
@@ -1,6 +1,5 @@
 package com.giodad.progetto_libreria.model;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,31 +11,29 @@ import lombok.NoArgsConstructor;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 
 public class Libro {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @NotBlank(message = "Il titolo non può essere in bianco")
-    @Size(min=3 , max=100 , message = "Lunghezza minima di 3 e lunghezza massima di 100")
-    private String titolo;
-    @NotBlank(message = "L'autore non può essere in bianco")
-    @Size(min=3 , max=100 , message = "Lunghezza minima di 3 e lunghezza massima di 100")
-    private String Autore;
-    @NotNull(message = "Il prezzo non può essere in bianco")
-    private Double prezzo;
-    
-    Libro(String titolo,String Autore, Double prezzo)
-    {
-      this.titolo= titolo;
-      this.Autore= Autore;
-      this.prezzo= prezzo;  
-    }
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
+  @NotBlank(message = "Il titolo non può essere in bianco")
+  @Size(min = 3, max = 100, message = "Lunghezza minima di 3 e lunghezza massima di 100")
+  private String titolo;
+  @NotBlank(message = "L'autore non può essere in bianco")
+  @Size(min = 3, max = 100, message = "Lunghezza minima di 3 e lunghezza massima di 100")
+  private String Autore;
+  @NotNull(message = "Il prezzo non può essere in bianco")
+  private Double prezzo;
+
+  Libro(String titolo, String Autore, Double prezzo) {
+    this.titolo = titolo;
+    this.Autore = Autore;
+    this.prezzo = prezzo;
+  }
 
 }
